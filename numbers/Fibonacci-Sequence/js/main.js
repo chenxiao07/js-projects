867d030600a22bfe7c10da8032c9701c0a8970c0
leungwensen <leungwensen@gmail.com>
2013-07-30 18:21:16 +0900

    done Fibonacci Sequence

---
+++ numbers/Fibonacci-Sequence/js/main.js
@@ -0,0 +1,56 @@
+;(function(){
+    window.addEventListener('load', function(){
+        function fibonacciSequence(n, type){
+            if (n < 0){
+                throw new Error('wrong length');
+            }
+            if (type === 1){
+                return fibonacciSequenceNonRecursion(n);
+            }
+            if (type === 2){
+                var resultArray = [], i;
+                for (i = 0; i <= n; i ++){
+                    resultArray[i] = fibonacciSequenceRecursion(i);
+                }
+                return resultArray;
+            }
+            throw new Error('wrong type');
+        }
+
+        function fibonacciSequenceNonRecursion(n){
+            if (n === 0){
+                return [0];
+            }
+            if (n === 1){
+                return [0, 1];
+            }
+            var result = [0, 1], i;
+            for (i = 2; i <= n; i ++){
+                result.push(result[i - 1] + result[i - 2]);
+            }
+            return result;
+        }
+
+        function fibonacciSequenceRecursion(n){
+            if (n === 0){
+                return 0;
+            }
+            if (n === 1){
+                return 1;
+            }
+            return fibonacciSequenceRecursion(n - 1)
+                 + fibonacciSequenceRecursion(n - 2);
+        }
+
+        document.getElementById('compute-nomal').addEventListener('click', function(){
+            var length = document.getElementById('length').value;
+            document.getElementById('result').innerHTML = '';
+            document.getElementById('result').innerHTML = (fibonacciSequence(length, 1).join(', '));
+        });
+        document.getElementById('compute-recursion').addEventListener('click', function(){
+            var length = document.getElementById('length').value;
+            document.getElementById('result').innerHTML = '';
+            document.getElementById('result').innerHTML = (fibonacciSequence(length, 2).join(', '));
+        });
+    });
+})();
