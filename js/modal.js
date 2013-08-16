0cbe43c95e7ed0b9300aad40b026e6fda9ead765
leungwensen <leungwensen@gmail.com>
2013-08-16 14:35:00 +0900

    use modal instead of popover (because of bug in popover)

---
+++ js/modal.js
@@ -0,0 +1,53 @@
+
+!function ($) {
+    var Logger = function(){
+        this.trigger = $('#show-modal-log');
+        this.content = $('#modal-log-content');
+    };
+    Logger.prototype = {
+        show : function(){
+            this.trigger.click();
+        },
+        clear : function(){
+            this.content.html('');
+            this.content.attr('class', '');
+        },
+        log : function(content, contentClass){
+            this.clear();
+            this.content.html(content);
+            if (contentClass !== undefined){
+                this.content.attr('class', contentClass);
+            }
+        }
+    };
+
+    var RSLoader = function(){
+        this.trigger = $('#show-modal-resource');
+        this.title   = $('#modal-resource-title');
+        this.content = $('#modal-resource-content');
+    };
+    RSLoader.prototype = {
+        show : function(){
+            this.trigger.click();
+        },
+        clear : function(){
+            this.content.html('');
+            this.content.attr('class', '');
+            this.title.html('resource');
+        },
+        load : function(content, title, contentClass){
+            this.clear();
+            this.content.html(content);
+            if (title !== undefined){
+                this.title.html(title);
+            }
+            if (contentClass !== undefined){
+                this.content.attr('class', contentClass);
+            }
+        }
+    };
+
+    window.Logger   = new Logger();
+    window.RSLoader = new RSLoader();
+}(window.jQuery);
+
