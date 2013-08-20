d942710bd1ceefebc81534a39ea81f862aeae36e
leungwensen <leungwensen@gmail.com>
2013-08-20 16:10:24 +0900

    add script/deploy/git-merge-and-push.sh

---
+++ script/deploy/git-merge-and-push.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+
+git checkout master
+git merge dev
+git push origin master
+git push github master
+
+git checkout gh-pages
+git merge master
+git push github gh-pages
+
+git checkout dev
