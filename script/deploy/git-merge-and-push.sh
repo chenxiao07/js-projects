bfc874ebaa4bec179a92262c4faf7721f53615e2
leungwensen <leungwensen@gmail.com>
2013-08-21 11:07:03 +0900

    fix script/deploy/git-merge-and-push.sh

--- script/deploy/git-merge-and-push.sh
+++ script/deploy/git-merge-and-push.sh
@@ -1,13 +1,31 @@
 #!/usr/bin/env sh
 
-git checkout master
-git merge dev
-git push origin master
-git push github master
+GIT=$(which git)
+SED=$(which sed)
 
-git checkout gh-pages
-git merge master
-git push github gh-pages
+REMOTES=("origin" "github")
 
-git checkout dev
-git push github dev
+CURRENT_BRANCH=$($GIT branch | $SED -n '/\* /s///p')
+DEVELOP_BRANCH="dev"
+DEPLOY_BRANCHES=("master", "gh-pages", "dev")
+
+function main {
+    # first merge current chagnes into develop branch
+    $GIT merge    $DEVELOPE_BRANCH
+    $GIT checkout $DEVELOPE_BRANCH
+    $GIT merge    $CURRENT_BRANCH
+
+    # then deploy changes to targe branches
+    for br in $DEPLOY_BRANCHES
+    do
+        $GIT checkout $br
+        $GIT merge    $DEVELOPE_BRANCH
+        for remote in $REMOTES
+        do
+            $GIT push $remote $br
+        done
+    done
+    echo "done deploying"
+}
+
+main
