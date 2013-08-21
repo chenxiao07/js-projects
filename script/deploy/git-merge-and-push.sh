b937b77429b3a6c4aeac47b67cdad956d29cab44
leungwensen <leungwensen@gmail.com>
2013-08-21 11:55:56 +0900

    add more debug messages

--- script/deploy/git-merge-and-push.sh
+++ script/deploy/git-merge-and-push.sh
@@ -4,10 +4,14 @@ git=$(which git)
 sed=$(which sed)
 
 current_branch=$($git branch | $sed -n '/\* /s///p')
+echo $current_branch
 develop_branch='dev'
+echo $develop_branch
 
 remotes=(origin github)
+echo ${remotes[@]}
 deploy_branches=(master gh-pages $current_branch $develop_branch)
+echo ${deploy_branches[@]}
 
 function main {
     echo "DEBUG: merge $current_branch into $develop_branch"
